235b9d16cef85e6d0e5ea7638a5790745633bc91
vanGalilea <z.galili@icloud.com>
2017-05-25 14:18:18 +0200

    Extra styling thumbs in home

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,4 +6,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     resize_to_fit(300, 300)
     cloudinary_transformation :quality => 80
   end
+
+  version :small_thumb, from_version: :thumbnail do
+    process resize_to_fill: [75, 75]
+  end
+
 end
