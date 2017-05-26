c2acc936431d392305e3f77524823a633313d59e
vanGalilea <z.galili@icloud.com>
2017-05-26 12:43:32 +0200

    Through Association: Views

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,17 @@
 class RegistrationsController < ApplicationController
   before_action :authenticate_user!
-  
-  def create
-    @registration = current_user.registrations.create(registration_params)
 
-    redirect_to @registration.event, notice: "Thank you for registrating!"
+  def create
+    @registration = current_user.registrations.new(registration_params.merge(event_id: params[:event_id]))
+    @registration.set_total_price
+    @registration.set_status
+    @registration.save
+    redirect_to @registration.event, notice: "Thank you for registering!"
   end
 
   private
 
   def registration_params
-    params.require(:registration).permit(:status, :guests_count, :price, :event_id)
+    params.require(:registration).permit(:guests_count)
   end
 end
--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,4 +1,12 @@
 class Registration < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  def set_status
+    event.price > 0 ? self.status = "pending" : self.status = "confirmed"
+  end
+
+  def set_total_price
+    self.price = event.price * guests_count
+  end
 end
