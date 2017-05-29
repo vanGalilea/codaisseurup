77287bf3c8cdedc1c1895146883294db215a72c4
vanGalilea <z.galili@icloud.com>
2017-05-29 11:15:20 +0200

    deploying 2

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dqmqi1nxq'
+  config.api_key = '268378945858468'
+  config.api_secret = 'o1MfaX75MNYgh6bYIeovmNlMhZ0'
+  config.cdn_subdomain = true
+end
