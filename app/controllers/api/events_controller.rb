76addb757c8a5b3034cd17a52e46e8787dc62bde
vanGalilea <z.galili@icloud.com>
2017-06-03 10:48:32 +0200

    Add API rooms destroy and update

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,11 +1,10 @@
 class Api::EventsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+  before_action :set_event, only: [:show, :update, :update, :destroy, :create]
 
   def show
-    event = Event.find(params[:id])
-
     render status: 200, json: {
-      event: event
+      event: @event
     }.to_json
   end
 
@@ -16,12 +15,10 @@ class Api::EventsController < ApplicationController
   end
 
   def create
-    event = Event.new(event_params)
-
-    if event.save
+    if @event.save
       render status: 201, json: {
         message: "Event successfully created",
-        event: event
+        event: @event
       }.to_json
     else
       render status: 422, json: {
@@ -30,9 +27,34 @@ class Api::EventsController < ApplicationController
     end
   end
 
+  def destroy
+    @event.destroy
+    render status: 200, json: {
+      message: "Event successfully deleted"
+    }.to_json
+  end
+
+  def update
+    if @event.update(event_params)
+      render status: 200, json: {
+        message: "Event successfully updated",
+        event: @event
+      }.to_json
+    else
+       render status: 422, json: {
+        message: "The event could not be updated",
+        errors: @event.errors
+      }.to_json
+    end
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, theme_ids: [])
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
